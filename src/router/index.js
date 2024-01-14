708539f2ad803ec5da4bfe6cdd22d33cd4ee7b09
Valentino Mettifogo <valentino.mettifogo@gmail.com>
2024-01-14 20:30:49 +0100

    handle 404 and fix routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '@/views/HomeView.vue'
 // import ContactView from '@/views/ContactView.vue'
 import MaintainanceViewVue from '@/views/MaintainanceView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,18 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: MaintainanceViewVue
+      // component: HomeView
     },
+    // {
+    //   path: '/contact',
+    //   name: 'contact',
+    //   component: ContactView
+    // },
     {
-      path: '/contact',
-      name: 'contact',
-      component: MaintainanceViewVue
-    }
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: NotFoundView,
+    },
   ]
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="mt-5 text-center">
+        <img src="/images/not-found.png" alt="Not Found!">
+    </div>
+</template>
