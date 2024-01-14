7bdce5dea06fdc01a6182ae82a802bdf394f59e0
Valentino Mettifogo <valentino.mettifogo@gmail.com>
2024-01-14 23:19:13 +0100

    add work section

--- src/components/WorkSection.vue
+++ src/components/WorkSection.vue
@@ -1,5 +1,76 @@
 <template>
     <div id="work-section">
-        <h3>Work Experience</h3>
-    </div>
-</template>
+        <section class="py-1 text-center">
+            <div class="row py-lg-5">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <h1 class="fw-light">My work exeprience</h1>
+                    <p class="lead text-body-secondary">Passionate Web and Business Intelligence Developer I'm specializing
+                        in Qlik's Software.
+                        With a keen eye for transforming raw data into meaningful insights,
+                        my expertise lies in the realms of ETL (Extract, Transform, Load) processes and crafting dynamic
+                        dashboards.</p>
+                    <p class="lead text-body-secondary">down here you can find some of the interesting things I do at work
+                        (I'll leave the boring ones for another time ok?)</p>
+                </div>
+            </div>
+        </section>
+
+        <div class="row row-cols-1 row-cols-md-3 g-3">
+
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="/images/placeholder.jpg" alt="Placeholder">
+                    <div class="card-body">
+                        <h5 class="card-title">ETL across different ERPs</h5>
+                        <p class="card-text">Collaborated with multiple teams to integrate different systems and data
+                            exchange</p>
+                        <!-- <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                            <small class="text-body-secondary">9 mins</small>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="/images/placeholder.jpg" alt="Placeholder">
+                    <div class="card-body">
+                        <h5 class="card-title">Qlik Sense Extensions</h5>
+                        <p class="card-text">Develop customize Qlik Sense Exteensions based on client requests and needed
+                        </p>
+                        <!-- <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                            <small class="text-body-secondary">9 mins</small>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="/images/placeholder.jpg" alt="Placeholder">
+                    <div class="card-body">
+                        <h5 class="card-title">Custom Reporting</h5>
+                        <p class="card-text">Strategic Data Distributor: Tailoring Insights Across Platforms and Audiences
+                        </p>
+                        <!-- <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                            <small class="text-body-secondary">9 mins</small>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+</div></template>
